bf81e88c93bc584a1414353a989f42e648565abf
a <a@DESKTOP-0G8F9IN>
2020-02-22 00:17:24 +0200

    first commit

---
+++ src/calculater/calculater.java
@@ -0,0 +1,16 @@
+package calculater;
+
+public class calculater {
+	public static double add2num(double a,double b) {
+    	return a+b;
+    	
+    }
+	public static void main(String[] args) {
+double a=5;
+double b=1.2;
+double c=add2num(a,b);
+System.out.print(c);
+	}
+
+}
+
